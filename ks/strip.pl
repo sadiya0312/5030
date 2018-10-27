6a5581847b533072c961e5be585494963b511d26
Kevin Scannell <kscanne@gmail.com>
2018-10-26 19:37:07 -0500

    Dr. Scannell's first cut

---
+++ ks/strip.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Unicode::Normalize;
+
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my $dbpath = '../data';
+my $testpath = '../tests';
+my %db;
+
+sub load_stopwords {
+  (my $language) = @_;
+  open(STOPWORDS, "<:utf8", "$dbpath/$language.txt") or die "Could not open stopword list for $language: $!";
+  while (<STOPWORDS>) {
+    chomp;
+    $db{$language}->{NFC($_)}=1;
+  }
+  close STOPWORDS;
+}
+
+sub strip_stopwords {
+  (my $language, my $string) = @_;
+  load_stopwords($language) unless (exists($db{$language}));
+  return join(' ', grep {!exists($db{$language}->{NFC($_)}) and !exists($db{$language}->{lc(NFC($_))})} split(/ +/,$string));
+}
+
+open(TESTS, "<:utf8", "$testpath/cases.tsv") or die "Could not open cases.tsv: $!";
+while (<TESTS>) {
+  chomp;
+  (my $testlang, my $testin, my $testout) = split(/\t/);
+  my $result = strip_stopwords($testlang,$testin);
+  print "Test on line $. failed; expected \"$testout\", got \"$result\"\n" unless $result eq $testout;
+}
+close TESTS;
+
+exit 0;
